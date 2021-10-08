63083377153be8a1ff70abf879403b0f594ba5c3
Uislan Cruz <uislansk8@hotmail.com>
2021-10-07 21:56:01 -0300

    ImparPar

--- ProgramaIdade/src/programaidade/ProgramaIdade.java
+++ ProgramaIdade/src/programaidade/ProgramaIdade.java
@@ -5,6 +5,7 @@
  */
 package programaidade;
 
+import java.util.Scanner;
 /**
  *
  * @author uisla
@@ -16,6 +17,19 @@ public class ProgramaIdade {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner t = new Scanner(System.in);
+        System.out.println("Em que ano você nasceu: ");
+        
+        int nasc = t.nextInt();
+        int i = 2021 - nasc;
+        System.out.println("Sua idade é: " +i);
+        if (i>=18){
+            System.out.println("Maior de Idade");
+        } else 
+        {
+            System.out.println("Menor de Idade");
+        }
+        
     }
     
 }
