446ab91d86ae03ada81eb737eb65765b900298f3
Uislan Cruz <uislansk8@hotmail.com>
2021-10-07 21:24:01 -0300

    Seg part

--- ProgramaMedia.java
+++ ProgramaMedia.java
@@ -19,6 +19,19 @@ public class ProgramaMedia {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner teclado = new Scanner(System.in);
+        float n1 = teclado.nextFloat();
+        float n2 = teclado.nextFloat();
+        float m = (n1 + n2)/2;
+        System.out.println("Sua média foi" +m);
+        if (m>9){
+            System.out.println("Parabens");
+              
+        }
+        else {
+            System.out.println("Reprovado");
+        }
+        
+        
     }
     
 }
